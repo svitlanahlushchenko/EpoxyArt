2b29a13d472a9a05366424074a5a29e071e7510d
Svitlana <shumovasveta27@gmail.com>
2025-04-15 12:03:27 +0200

    added burger

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,34 +1,90 @@
-import styles from './Header.module.scss';
+import { useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import styles from './Header.module.scss';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
+  // Закриття меню при кліку поза межами
+  useEffect(() => {
+    const handleClickOutside = e => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setMenuOpen(false);
+      }
+    };
+
+    if (menuOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuOpen]);
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <NavLink to='/' className='logoLink'>
+        <NavLink to='/' className={styles.logoLink}>
           <img
             src='/images/logo.png'
             alt='EpoxyArt Logo'
-            className='logoImage'
+            className={styles.logoImage}
           />
-          <span className='logoText'>EpoxyArt</span>
+          <span className={styles.logoText}>EpoxyArt</span>
         </NavLink>
 
-        <ul className={styles.menu}>
+        <button className={styles.burger} onClick={toggleMenu}>
+          <span />
+          <span />
+          <span />
+        </button>
+
+        <ul
+          className={`${styles.menu} ${menuOpen ? styles.open : ''}`}
+          ref={menuRef}
+        >
           <li>
-            <NavLink to='/' className={styles.link}>
+            <NavLink
+              to='/'
+              className={styles.link}
+              onClick={() => setMenuOpen(false)}
+            >
               Domov
             </NavLink>
           </li>
           <li>
-            <NavLink to='/learn' className={styles.link}>
+            <NavLink
+              to='/learn'
+              className={styles.link}
+              onClick={() => setMenuOpen(false)}
+            >
               Výuka
             </NavLink>
           </li>
           <li>
-            <NavLink to='/gallery' className={styles.link}>
+            <NavLink
+              to='/gallery'
+              className={styles.link}
+              onClick={() => setMenuOpen(false)}
+            >
               Galéria
             </NavLink>
+          </li>
+          <li>
+            <button
+              className={styles.closeButton}
+              onClick={() => setMenuOpen(false)}
+            >
+              ✕
+            </button>
           </li>
         </ul>
       </nav>
